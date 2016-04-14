8baf655bdb7c08cf9d101d965ea13047340f93ff
caseyagil <caseyagil@gmail.com>
2016-04-14 15:06:24 -0400

    components of the auth navbar and functions

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,32 @@
+'use strict';
+
+ angular.module('votingApp')
+   .controller('NavbarCtrl', function($scope, $location, Auth) {
+     $scope.menu = [{
+       'title': 'Home',
+       'link': '/',
+       'requiresLogin': false
+     }, {
+       'title': 'Create a New Poll',
+       'link': 'create-poll',
+       'requiresLogin': true
+     }, {
+       'title': 'My Polls',
+       'link': 'my-polls',
+       'requiresLogin': true
+     }];
+
+     $scope.isCollapsed = true;
+     $scope.isLoggedIn = Auth.isLoggedIn;
+     $scope.isAdmin = Auth.isAdmin;
+     $scope.getCurrentUser = Auth.getCurrentUser;
+
+     $scope.logout = function() {
+       Auth.logout();
+       $location.path('/login');
+     };
+
+     $scope.isActive = function(route) {
+       return route === $location.path();
+     };
+   });
